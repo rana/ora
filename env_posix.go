2124094ec2fce54893bb6b43967e5c6babccba72
Tamás Gulácsi <tgulacsi78@gmail.com>
2015-05-19 20:14:48 +0200

    Add #cgo LDFLAGS: -lclntsh for building on Linux
    And #cgo LDFLAGS: -loci for building on windows (maybe this is not
    necessary, I don't know).

---
+++ env_posix.go
@@ -0,0 +1,10 @@
+// +build !windows
+
+// Copyright 2015 Tamás Gulácsi. All rights reserved.
+// Use of this source code is governed by The MIT License
+// found in the accompanying LICENSE file.
+
+package ora
+
+// #cgo LDFLAGS: -lclntsh
+import "C"
