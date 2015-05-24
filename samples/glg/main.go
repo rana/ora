6c0e378012067827a50a9e9f8bc03ab01dc4465b
ranaian <rana.ian@gmail.com>
2015-05-23 18:05:36 -0700

    revised comment

--- samples/glg/main.go
+++ samples/glg/main.go
@@ -10,6 +10,21 @@ import (
 	"github.com/ranaian/ora/glg"
 )
 
+// Sample logging produced by glog
+//
+//	I0523 17:31:41.702365   97708 drv.go:411] OpenEnv 1
+//	I0523 17:31:41.728377   97708 drv.go:411] OpenEnv 2
+//	I0523 17:31:41.728377   97708 env.go:115] E2] OpenSrv (dbname orcl)
+//	I0523 17:31:41.741390   97708 env.go:150] E2] OpenSrv (srvId 1)
+//	I0523 17:31:41.741390   97708 srv.go:113] E2S1] OpenSes (username test)
+//	I0523 17:31:41.762366   97708 ses.go:163] E2S1S1] Prep: SELECT CURRENT_TIMESTAMP FROM DUAL
+//	I0523 17:31:41.762366   97708 rset.go:205] E2S1S1S1R0] open
+//	I0523 17:31:41.762366   97708 ses.go:74] E2S1S1] Close
+//	I0523 17:31:41.762366   97708 stmt.go:78] E2S1S1S1] Close
+//	I0523 17:31:41.762366   97708 rset.go:57] E2S1S1S1R0] close
+//	I0523 17:31:41.763365   97708 srv.go:63] E2S1] Close
+//	I0523 17:31:41.763365   97708 env.go:68] E2] Close
+//
 func main() {
 
 	// parse flags for glog (required)
