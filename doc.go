2f6ac858289dfb6975b603f271a0fccdc059ce65
ranaian <rana.ian@gmail.com>
2015-05-23 17:56:16 -0700

    revised documentation

--- doc.go
+++ doc.go
@@ -34,11 +34,9 @@ the location of the OCI library. These locations will vary based on whether an O
 database is locally installed or whether the Oracle instant client libraries are
 locally installed.
 
-The ora package uses glog for logging. The ora package and glog package are available
-from GitHub:
+The ora package has no external Go dependencies and is available on GitHub:
 
 	go get github.com/ranaian/ora
-	go get github.com/golang/glog
 
 Data Types
 
@@ -783,7 +781,98 @@ to Version requires an open Ses:
 		fmt.Println("Received version from server")
 	}
 
-Further code examples are available in the samples folder, example file and test files.
+Further code examples are available in the samples folder
+https://github.com/ranaian/ora/tree/master/samples and test files.
+
+Logging
+
+The ora package uses the Go log package to write to os.Stderr by default. A
+sample of the default ora logging:
+
+	ORA I 2015/05/23 16:54:44.615462 drv.go:411: OpenEnv 1
+	ORA I 2015/05/23 16:54:44.626443 drv.go:411: OpenEnv 2
+	ORA I 2015/05/23 16:54:44.627465 env.go:115: E2] OpenSrv (dbname orcl)
+	ORA I 2015/05/23 16:54:44.643449 env.go:150: E2] OpenSrv (srvId 1)
+	ORA I 2015/05/23 16:54:44.643449 srv.go:113: E2S1] OpenSes (username test)
+	ORA I 2015/05/23 16:54:44.665451 ses.go:163: E2S1S1] Prep: SELECT CURRENT_TIMESTAMP FROM DUAL
+	ORA I 2015/05/23 16:54:44.666451 rset.go:205: E2S1S1S1R0] open
+	ORA I 2015/05/23 16:54:44.666451 ses.go:74: E2S1S1] Close
+	ORA I 2015/05/23 16:54:44.666451 stmt.go:78: E2S1S1S1] Close
+	ORA I 2015/05/23 16:54:44.666451 rset.go:57: E2S1S1S1R0] close
+	ORA I 2015/05/23 16:54:44.666451 srv.go:63: E2S1] Close
+	ORA I 2015/05/23 16:54:44.667451 env.go:68: E2] Close
+
+Default logging statements are prefixed with 'ORA I' for information or 'ORA E'
+for an error.
+
+A simple ora.Logger interface is available to plug-in a logging package of your
+choice. Currently, the optional glog package and the optional log15 package have
+built-in support.
+
+To use the optional glog package:
+
+	import (
+		"flag"
+		"github.com/ranaian/ora"
+		"github.com/ranaian/ora/glg"
+	)
+
+	func main() {
+
+		// parse flags for glog (required)
+		// consider specifying cmd line arg -alsologtostderr=true
+		flag.Parse()
+
+		// use the optional glog package for ora logging
+		ora.Log = glg.Log
+	}
+
+which produces a sample log of:
+
+	I0523 17:31:41.702365   97708 drv.go:411] OpenEnv 1
+	I0523 17:31:41.728377   97708 drv.go:411] OpenEnv 2
+	I0523 17:31:41.728377   97708 env.go:115] E2] OpenSrv (dbname orcl)
+	I0523 17:31:41.741390   97708 env.go:150] E2] OpenSrv (srvId 1)
+	I0523 17:31:41.741390   97708 srv.go:113] E2S1] OpenSes (username test)
+	I0523 17:31:41.762366   97708 ses.go:163] E2S1S1] Prep: SELECT CURRENT_TIMESTAMP FROM DUAL
+	I0523 17:31:41.762366   97708 rset.go:205] E2S1S1S1R0] open
+	I0523 17:31:41.762366   97708 ses.go:74] E2S1S1] Close
+	I0523 17:31:41.762366   97708 stmt.go:78] E2S1S1S1] Close
+	I0523 17:31:41.762366   97708 rset.go:57] E2S1S1S1R0] close
+	I0523 17:31:41.763365   97708 srv.go:63] E2S1] Close
+	I0523 17:31:41.763365   97708 env.go:68] E2] Close
+
+See https://github.com/ranaian/ora/tree/master/samples/glg for sample code which
+uses glog.
+
+To use the optional log15 package:
+
+	import (
+		"github.com/ranaian/ora"
+		"github.com/ranaian/ora/lg15"
+	)
+	func main() {
+		// use the optional log15 package for logging
+		ora.Log = lg15.Log
+	}
+
+which produces a sample log of:
+
+	t=2015-05-23T17:08:32-0700 lvl=info msg="OpenEnv 1" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="OpenEnv 2" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2] OpenSrv (dbname orcl)" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2] OpenSrv (srvId 1)" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2S1] OpenSes (username test)" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2S1S1] Prep: SELECT CURRENT_TIMESTAMP FROM DUAL" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2S1S1S1R0] open" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2S1S1] Close" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2S1S1S1] Close" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2S1S1S1R0] close" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2S1] Close" lib=ora
+	t=2015-05-23T17:08:32-0700 lvl=info msg="E2] Close" lib=ora
+
+See https://github.com/ranaian/ora/tree/master/samples/lg15 for sample code which
+uses log15.
 
 Test Database Setup
 
