40d39415566ec81df6845918fc1a59ec47b328a1
rana <rana_ian@gmail.com>
2015-06-28 17:26:25 -0700

    revised error recovery method

--- bndFloat64.go
+++ bndFloat64.go
@@ -56,7 +56,7 @@ func (bnd *bndFloat64) setPtr() error {
 func (bnd *bndFloat64) close() (err error) {
 	defer func() {
 		if value := recover(); value != nil {
-			err = errRecover(value)
+			err = errR(value)
 		}
 	}()
 
