a261b8a8d0b1181967f83fab4defcdd4f6e699ed
rana <rana_ian@gmail.com>
2015-06-28 17:28:06 -0700

    revised error recovery method

--- bndInt8.go
+++ bndInt8.go
@@ -57,7 +57,7 @@ func (bnd *bndInt8) setPtr() error {
 func (bnd *bndInt8) close() (err error) {
 	defer func() {
 		if value := recover(); value != nil {
-			err = errRecover(value)
+			err = errR(value)
 		}
 	}()
 
