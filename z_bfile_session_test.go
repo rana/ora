a95bb6668f7672d9cfd282eaf852dca9a21956f1
rana <rana_ian@gmail.com>
2015-06-28 17:38:24 -0700

    minor test revision

--- z_bfile_session_test.go
+++ z_bfile_session_test.go
@@ -16,13 +16,16 @@ import (
 // bfile
 ////////////////////////////////////////////////////////////////////////////////
 func TestBindDefine_OraBfile_bfile_session(t *testing.T) {
+	//enableLogging(t)
 	testBindDefine(gen_OraBfile(false), bfile, t, nil)
 }
 
 func TestBindDefine_OraBfileNull_bfile_session(t *testing.T) {
+	//enableLogging(t)
 	testBindDefine(gen_OraBfile(false), bfileNull, t, nil)
 }
 
 func TestBindDefine_OraBfileNull_bfile_null_session(t *testing.T) {
+	//enableLogging(t)
 	testBindDefine(gen_OraBfile(true), bfileNull, t, nil)
 }
