c4d9a9294a72f761ea50de1179d5447c44a997aa
Tamás Gulácsi <tgulacsi78@gmail.com>
2015-07-26 07:33:49 +0200

    bndStringPtr: use alen and propagate buf

--- bndStringPtr.go
+++ bndStringPtr.go
@@ -18,29 +18,51 @@ type bndStringPtr struct {
 	ocibnd *C.OCIBind
 	isNull C.sb2
 	value  *string
+	alen   []C.ACTUAL_LENGTH_TYPE
 	buf    []byte
 }
 
 func (bnd *bndStringPtr) bind(value *string, position int, stringPtrBufferSize int, stmt *Stmt) error {
 	bnd.stmt = stmt
 	bnd.value = value
+	var length int
 	if cap(bnd.buf) < stringPtrBufferSize {
-		bnd.buf = make([]byte, stringPtrBufferSize)
+		bnd.buf = make([]byte, 1, stringPtrBufferSize)
 	}
+	if value == nil {
+		bnd.isNull = C.sb2(-1)
+	} else {
+		length = len(*value)
+	}
+	if length == 0 {
+		bnd.buf = bnd.buf[:1] // to be able to address bnd.buf[0]
+		bnd.buf[0] = 0
+	} else {
+		bnd.buf = bnd.buf[:length]
+		copy(bnd.buf, []byte(*value))
+	}
+	if cap(bnd.alen) < 1 {
+		bnd.alen = []C.ACTUAL_LENGTH_TYPE{C.ACTUAL_LENGTH_TYPE(length)}
+	} else {
+		bnd.alen = bnd.alen[:1]
+		bnd.alen[0] = C.ACTUAL_LENGTH_TYPE(length)
+	}
+	bnd.stmt.logF(_drv.cfg.Log.Stmt.Bind,
+		"StringPtr.bind(%d) cap=%d len=%d alen=%d", position, cap(bnd.buf), len(bnd.buf), bnd.alen[0])
 	r := C.OCIBINDBYPOS(
 		bnd.stmt.ocistmt,            //OCIStmt      *stmtp,
 		(**C.OCIBind)(&bnd.ocibnd),  //OCIBind      **bindpp,
 		bnd.stmt.ses.srv.env.ocierr, //OCIError     *errhp,
 		C.ub4(position),             //ub4          position,
 		unsafe.Pointer(&bnd.buf[0]), //void         *valuep,
-		C.LENGTH_TYPE(len(bnd.buf)), //sb8          value_sz,
+		C.LENGTH_TYPE(cap(bnd.buf)), //sb8          value_sz,
 		C.SQLT_CHR,                  //ub2          dty,
 		unsafe.Pointer(&bnd.isNull), //void         *indp,
-		nil,           //ub2          *alenp,
-		nil,           //ub2          *rcodep,
-		0,             //ub4          maxarr_len,
-		nil,           //ub4          *curelep,
-		C.OCI_DEFAULT) //ub4          mode );
+		&bnd.alen[0],                //ub2          *alenp,
+		nil,                         //ub2          *rcodep,
+		0,                           //ub4          maxarr_len,
+		nil,                         //ub4          *curelep,
+		C.OCI_DEFAULT)               //ub4          mode );
 	if r == C.OCI_ERROR {
 		return bnd.stmt.ses.srv.env.ociError()
 	}
@@ -48,24 +70,28 @@ func (bnd *bndStringPtr) bind(value *string, position int, stringPtrBufferSize i
 }
 
 func (bnd *bndStringPtr) setPtr() error {
+	bnd.stmt.logF(_drv.cfg.Log.Stmt.Bind,
+		"StringPtr.setPtr isNull=%d alen=%d", bnd.isNull, bnd.alen[0])
 	if bnd.isNull > C.sb2(-1) {
-		// Buffer is padded with Space char (32)
-		*bnd.value = stringTrimmed(bnd.buf, 32)
+		*bnd.value = string(bnd.buf[:bnd.alen[0]])
 	}
 	return nil
 }
 
 func (bnd *bndStringPtr) close() (err error) {
-	defer func() {
-		if value := recover(); value != nil {
-			err = errR(value)
-		}
-	}()
+	/*
+		defer func() {
+			if value := recover(); value != nil {
+				err = errR(value)
+			}
+		}()
+	*/
 	stmt := bnd.stmt
 	bnd.stmt = nil
 	bnd.ocibnd = nil
 	bnd.value = nil
-	clear(bnd.buf, 32)
+	bnd.alen = bnd.alen[:0]
+	bnd.buf = bnd.buf[:0]
 	stmt.putBnd(bndIdxStringPtr, bnd)
 	return nil
 }
