rana/ora

commit ae7ffb3ace603954d5fc241e3a777bd5fe978ee3

Eliminate stringTrimmed
It is not needed, as the length is returned in a pointer.
This helps performance for SELECTing strings.

defString.go
@@ -17,6 +17,7 @@ type defString struct {
 	null       C.sb2
 	isNullable bool
 	buf        []byte
+	rlen       C.ACTUAL_LENGTH_TYPE
 }
 
 func (def *defString) define(position int, columnSize int, isNullable bool, rset *Rset) error {
@@ -52,9 +53,9 @@ func (def *defString) define(position int, columnSize int, isNullable bool, rset
 		C.LENGTH_TYPE(n),                 //sb8         value_sz,
 		C.SQLT_CHR,                       //ub2         dty,
 		unsafe.Pointer(&def.null),        //void        *indp,
-		nil,           //ub2         *rlenp,
-		nil,           //ub2         *rcodep,
-		C.OCI_DEFAULT) //ub4         mode );
+		&def.rlen,                        //ub2         *rlenp,
+		nil,                              //ub2         *rcodep,
+		C.OCI_DEFAULT)                    //ub4         mode );
 	if r == C.OCI_ERROR {
 		return def.rset.stmt.ses.srv.env.ociError()
 	}
@@ -66,14 +67,14 @@ func (def *defString) value() (value interface{}, err error) {
 	if def.isNullable {
 		oraStringValue := String{IsNull: def.null < C.sb2(0)}
 		if !oraStringValue.IsNull {
-			oraStringValue.Value = stringTrimmed(def.buf, 32)
+			oraStringValue.Value = string(def.buf[:int(def.rlen)])
 		}
 		value = oraStringValue
 	} else {
 		if def.null < C.sb2(0) {
 			value = ""
 		} else {
-			value = stringTrimmed(def.buf, 32)
+			value = def.buf[:int(def.rlen)]
 		}
 	}
 	return value, err

util.go
@@ -129,25 +129,8 @@ func GctName(gct GoColumnType) string {
 	return ""
 }
 
-func stringTrimmed(buffer []byte, pad byte) string {
-	// Find length of non-padded string value
-	// String buffer returned from Oracle is padded with Space char (32)
-	//fmt.Println("stringTrimmed: len(buffer): ", len(buffer))
-	var n int
-	for n = len(buffer) - 1; n > -1; n-- {
-		if buffer[n] != pad {
-			n++
-			break
-		}
-	}
-	if n > 0 {
-		return string(buffer[:n])
-	}
-	return ""
-}
-
 func clear(buffer []byte, fill byte) {
-	for n, _ := range buffer {
+	for n := range buffer {
 		buffer[n] = fill
 	}
 }

commit f7cd95cfd749e11857f50e5cd538b76413a22c0f

defString.value must return string
Fix an error introduced in ae7ffb3ace603954d5fc241e3a777bd5fe978ee3
that returned []byte for non-nullable strings.

defString.go
@@ -63,21 +63,17 @@ func (def *defString) define(position int, columnSize int, isNullable bool, rset
 }
 
 func (def *defString) value() (value interface{}, err error) {
-	// Buffer is padded with Space char (32)
 	if def.isNullable {
 		oraStringValue := String{IsNull: def.null < C.sb2(0)}
 		if !oraStringValue.IsNull {
 			oraStringValue.Value = string(def.buf[:int(def.rlen)])
 		}
-		value = oraStringValue
-	} else {
-		if def.null < C.sb2(0) {
-			value = ""
-		} else {
-			value = def.buf[:int(def.rlen)]
-		}
+		return oraStringValue, nil
 	}
-	return value, err
+	if def.null < C.sb2(0) {
+		return "", nil
+	}
+	return string(def.buf[:int(def.rlen)]), nil
 }
 
 func (def *defString) alloc() error {