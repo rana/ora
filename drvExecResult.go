rana/ora

commit a701820877c273c8ba6eaf6f2289e58d87254905

LastInsertId iff specified with alias

drvExecResult.go
@@ -28,7 +28,7 @@ type DrvExecResult struct {
 //
 //	db.Exec("CREATE TABLE T1 (C1 NUMBER(19,0) GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), C2 VARCHAR2(48 CHAR))")
 //
-//	result, err := db.Exec("INSERT INTO T1 (C2) VALUES ('GO') RETURNING C1 INTO :C1", nil)
+//	result, err := db.Exec("INSERT INTO T1 (C2) VALUES ('GO') RETURNING C1 lastInsertId INTO :C1", nil)
 //
 //	id, err := result.LastInsertId()
 func (er *DrvExecResult) LastInsertId() (int64, error) {

stmt.go
@@ -150,6 +150,8 @@ func (stmt *Stmt) Exe(params ...interface{}) (rowsAffected uint64, err error) {
 	return rowsAffected, err
 }
 
+var spcRpl = strings.NewReplacer("\t", " ", "   ", " ", "  ", " ")
+
 // exe executes a SQL statement on an Oracle server returning rowsAffected, lastInsertId and error.
 func (stmt *Stmt) exe(params []interface{}) (rowsAffected uint64, lastInsertId int64, err error) {
 	stmt.mu.Lock()
@@ -167,10 +169,10 @@ func (stmt *Stmt) exe(params []interface{}) (rowsAffected uint64, lastInsertId i
 	// for case of inserting and returning identity for database/sql package
 	if _drv.sqlPkgEnv == stmt.ses.srv.env && stmt.stmtType == C.OCI_STMT_INSERT {
 		lastIndex := strings.LastIndex(stmt.sql, ")")
-		sqlEnd := stmt.sql[lastIndex+1 : len(stmt.sql)]
+		sqlEnd := spcRpl.Replace(stmt.sql[lastIndex+1 : len(stmt.sql)])
 		sqlEnd = strings.ToUpper(sqlEnd)
 		// add *int64 arg to capture identity
-		if strings.Contains(sqlEnd, "RETURNING") {
+		if i := strings.LastIndex(sqlEnd, "RETURNING"); i >= 0 && strings.Contains(sqlEnd[i:], " LASTINSERTID INTO ") {
 			params[len(params)-1] = &lastInsertId
 		}
 	}
@@ -190,7 +192,7 @@ func (stmt *Stmt) exe(params []interface{}) (rowsAffected uint64, lastInsertId i
 	}
 	// Execute statement on Oracle server
 	r := C.OCIStmtExecute(
-		stmt.ses.ocisvcctx,  //OCISvcCtx           *svchp,
+		stmt.ses.ocisvcctx,      //OCISvcCtx           *svchp,
 		stmt.ocistmt,            //OCIStmt             *stmtp,
 		stmt.ses.srv.env.ocierr, //OCIError            *errhp,
 		C.ub4(iterations),       //ub4                 iters,
@@ -249,7 +251,7 @@ func (stmt *Stmt) qry(params []interface{}) (rset *Rset, err error) {
 	}
 	// Query statement on Oracle server
 	r := C.OCIStmtExecute(
-		stmt.ses.ocisvcctx,  //OCISvcCtx           *svchp,
+		stmt.ses.ocisvcctx,      //OCISvcCtx           *svchp,
 		stmt.ocistmt,            //OCIStmt             *stmtp,
 		stmt.ses.srv.env.ocierr, //OCIError            *errhp,
 		C.ub4(0),                //ub4                 iters,

z_db_test.go
@@ -89,7 +89,7 @@ func Test_numberP38S0Identity_db(t *testing.T) {
 	}
 	defer dropTableDB(testDb, t, tableName)
 
-	stmt, err = testDb.Prepare(fmt.Sprintf("insert into %v (c2) values ('go') returning c1 into :c1", tableName))
+	stmt, err = testDb.Prepare(fmt.Sprintf("insert into %v (c2) values ('go') returning c1 lastInsertId into :c1", tableName))
 	defer stmt.Close()
 
 	// pass nil to Exec when using 'returning into' clause with sql.DB

ses.go
@@ -368,10 +368,10 @@ func (ses *Ses) Ins(tbl string, columnPairs ...interface{}) (err error) {
 	buf.WriteString(lastColName)
 	buf.WriteString(" INTO :RET_VAL")
 	stmt, err := ses.Prep(buf.String()) // prep
-	defer stmt.Close()
 	if err != nil {
 		return errE(err)
 	}
+	defer stmt.Close()
 	_, err = stmt.Exe(params...) // exe
 	if err != nil {
 		return errE(err)

z_example_test.go
@@ -60,7 +60,7 @@ func ExampleDrvStmt_Exec_insert_return_identity() {
 
 	// use a 'returning into' SQL clause and specify a nil parameter to Exec
 	// placeholder ':c1' is bound by position; ':c1' may be any name
-	result, err := db.Exec(fmt.Sprintf("insert into %v (c2) values ('go') returning c1 into :c1", tableName), nil)
+	result, err := db.Exec(fmt.Sprintf("insert into %v (c2) values ('go') returning c1 lastinsertid into :c1", tableName), nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error inserting 'go' with returning: %v", err)
 		return

commit 5c6918310b33fea370c6ff6aa5cc4f2afe47d5aa

RETURNING .. INTO does not allow column alias
This error was introduced with a701820877c273c8ba6eaf6f2289e58d87254905,
where we wanted to make the use of LastInsertId more explicit, and don't
mess with other RETURNING cases.

drvExecResult.go
@@ -28,7 +28,7 @@ type DrvExecResult struct {
 //
 //	db.Exec("CREATE TABLE T1 (C1 NUMBER(19,0) GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), C2 VARCHAR2(48 CHAR))")
 //
-//	result, err := db.Exec("INSERT INTO T1 (C2) VALUES ('GO') RETURNING C1 lastInsertId INTO :C1", nil)
+//	result, err := db.Exec("INSERT INTO T1 (C2) VALUES ('GO') RETURNING C1 /*lastInsertId*/ INTO :C1", nil)
 //
 //	id, err := result.LastInsertId()
 func (er *DrvExecResult) LastInsertId() (int64, error) {

stmt.go
@@ -172,7 +172,7 @@ func (stmt *Stmt) exe(params []interface{}) (rowsAffected uint64, lastInsertId i
 		sqlEnd := spcRpl.Replace(stmt.sql[lastIndex+1 : len(stmt.sql)])
 		sqlEnd = strings.ToUpper(sqlEnd)
 		// add *int64 arg to capture identity
-		if i := strings.LastIndex(sqlEnd, "RETURNING"); i >= 0 && strings.Contains(sqlEnd[i:], " LASTINSERTID INTO ") {
+		if i := strings.LastIndex(sqlEnd, "RETURNING"); i >= 0 && strings.Contains(sqlEnd[i:], " /*LASTINSERTID*/ INTO ") {
 			params[len(params)-1] = &lastInsertId
 		}
 	}

z_db_test.go
@@ -89,7 +89,7 @@ func Test_numberP38S0Identity_db(t *testing.T) {
 	}
 	defer dropTableDB(testDb, t, tableName)
 
-	stmt, err = testDb.Prepare(fmt.Sprintf("insert into %v (c2) values ('go') returning c1 lastInsertId into :c1", tableName))
+	stmt, err = testDb.Prepare(fmt.Sprintf("insert into %v (c2) values ('go') returning c1 /*lastInsertId*/ into :c1", tableName))
 	defer stmt.Close()
 
 	// pass nil to Exec when using 'returning into' clause with sql.DB