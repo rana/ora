b8a287db0594b04ffd05958825d763a3387e7fe6
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-03-18 07:52:25 +0100

    start ocinum

---
+++ ocinum.go
@@ -0,0 +1,71 @@
+// Copyright 2016 Tamás Gulácsi. All rights reserved.
+// Use of this source code is governed by The MIT License
+// found in the accompanying LICENSE file.
+
+package ora
+
+/*
+#include <oci.h>
+#include <stdlib.h>
+*/
+import "C"
+import "sync"
+
+// Num is an OCINumber
+//
+// SQLT_VNU: 22 bytes, at max.
+// SQLT_NUM: 21 bytes
+//
+// http://docs.oracle.com/cd/B28359_01/appdev.111/b28395/oci03typ.htm#sthref365
+//
+/*
+Oracle stores values of the NUMBER datatype in a variable-length format. The first byte is the exponent and is followed by 1 to 20 mantissa bytes. The high-order bit of the exponent byte is the sign bit; it is set for positive numbers and it is cleared for negative numbers. The lower 7 bits represent the exponent, which is a base-100 digit with an offset of 65.
+
+To calculate the decimal exponent, add 65 to the base-100 exponent and add another 128 if the number is positive. If the number is negative, you do the same, but subsequently the bits are inverted. For example, -5 has a base-100 exponent = 62 (0x3e). The decimal exponent is thus (~0x3e) -128 - 65 = 0xc1 -128 -65 = 193 -128 -65 = 0.
+
+Each mantissa byte is a base-100 digit, in the range 1..100. For positive numbers, the digit has 1 added to it. So, the mantissa digit for the value 5 is 6. For negative numbers, instead of adding 1, the digit is subtracted from 101. So, the mantissa digit for the number -5 is 96 (101 - 5). Negative numbers have a byte containing 102 appended to the data bytes. However, negative numbers that have 20 mantissa bytes do not have the trailing 102 byte. Because the mantissa digits are stored in base 100, each byte can represent 2 decimal digits. The mantissa is normalized; leading zeroes are not stored.
+
+Up to 20 data bytes can represent the mantissa. However, only 19 are guaranteed to be accurate. The 19 data bytes, each representing a base-100 digit, yield a maximum precision of 38 digits for an Oracle NUMBER.
+
+If you specify the datatype code 2 in the dty parameter of an OCIDefineByPos() call, your program receives numeric data in this Oracle internal format. The output variable should be a 21-byte array to accommodate the largest possible number. Note that only the bytes that represent the number are returned. There is no blank padding or NULL termination. If you need to know the number of bytes returned, use the VARNUM external datatype instead of NUMBER.
+*/
+type Num []byte
+
+// Print the number into the given byte slice.
+func (num Num) Print(buf []byte) []byte {
+	res := buf[:0]
+	b := num[0]
+	negative := b&(1<<7) == 0
+	exp := int16(b & ((1 << 7) - 1))
+	if negative {
+		res = append(res, '-')
+		exp = ^exp - 128 - 65
+		length := len(num)
+		if num[length-1] == 102 {
+			length--
+		}
+		for _, b := range num[:length] {
+			res = append(res, '0'+(101-b))
+		}
+	} else {
+		exp += 65 + 128
+		for _, b := range num {
+			res = append(res, '0'+(b-1))
+		}
+	}
+	return res
+}
+
+var bytesPool = sync.Pool{New: func() interface{} { return make([]byte, 0, 4) }}
+
+// String returns the string representation of the number.
+func (num Num) String() string {
+	b := bytesPool.Get().([]byte)
+	s := string(num.Print(b))
+	bytesPool.Put(b)
+	return s
+}
+
+// SetString sets the Num to the number in s.
+func (num *Num) SetString(s string) error {
+}
