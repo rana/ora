694b0b831775fa9f9f1066b61d665068e0f0a1c1
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-04-19 09:49:19 +0200

    add Debugf, Infof global logging functions

--- logger.go
+++ logger.go
@@ -4,6 +4,11 @@
 
 package ora
 
+var (
+	Infof  func(format string, args ...interface{})
+	Debugf func(format string, args ...interface{})
+)
+
 // Logger interface is for logging.
 type Logger interface {
 	Infof(format string, args ...interface{})
@@ -14,7 +19,27 @@ type Logger interface {
 
 type EmpLgr struct{}
 
-func (e EmpLgr) Infof(format string, v ...interface{})  {}
-func (e EmpLgr) Infoln(v ...interface{})                {}
-func (e EmpLgr) Errorf(format string, v ...interface{}) {}
-func (e EmpLgr) Errorln(v ...interface{})               {}
+func (e EmpLgr) Infof(format string, v ...interface{}) {
+	if Debugf == nil {
+		return
+	}
+	Debugf(format, v)
+}
+func (e EmpLgr) Infoln(v ...interface{}) {
+	if Debugf == nil {
+		return
+	}
+	Debugf("%v", v)
+}
+func (e EmpLgr) Errorf(format string, v ...interface{}) {
+	if Infof == nil {
+		return
+	}
+	Infof(format, v)
+}
+func (e EmpLgr) Errorln(v ...interface{}) {
+	if Infof == nil {
+		return
+	}
+	Infof("%v", v)
+}
