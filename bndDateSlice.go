rana/ora

commit 6dd783f22d88e70d0a184e8f22e1596252b8f01c

still one more bool test error

bndTimeSlice.go
@@ -153,12 +153,6 @@ func (bnd *bndTimeSlice) setPtr() error {
 }
 
 func (bnd *bndTimeSlice) free(n int) {
-	defer func() {
-		recover()
-	}()
-	C.OCIDescriptorFree(
-		unsafe.Pointer(bnd.ociDateTimes[n]), //void     *descp,
-		C.OCI_DTYPE_TIMESTAMP_TZ)            //ub4      type );
 }
 
 func (bnd *bndTimeSlice) close() (err error) {
@@ -169,7 +163,10 @@ func (bnd *bndTimeSlice) close() (err error) {
 	}()
 
 	for n := range bnd.ociDateTimes {
-		bnd.free(n)
+		arr := bnd.ociDateTimes[n : n+1 : 1]
+		C.OCIDescriptorFree(
+			unsafe.Pointer(&arr[0]),  //void     *descp,
+			C.OCI_DTYPE_TIMESTAMP_TZ) //ub4      type );
 	}
 	stmt := bnd.stmt
 	bnd.stmt = nil

defTime.go
@@ -78,14 +78,6 @@ func (def *defTime) alloc() error {
 }
 
 func (def *defTime) free() {
-	defer func() {
-		recover()
-	}()
-	if dt := def.dateTimep.Value(); dt != nil {
-		C.OCIDescriptorFree(
-			unsafe.Pointer(dt),       //void     *descp,
-			C.OCI_DTYPE_TIMESTAMP_TZ) //ub4      type );
-	}
 }
 
 func (def *defTime) close() (err error) {

z_bool_session_test.go
@@ -4,7 +4,11 @@
 
 package ora_test
 
-import "testing"
+import (
+	"testing"
+
+	"gopkg.in/rana/ora.v3"
+)
 
 //// string or bool
 //charB1     oracleColumnType = "char(1 byte) not null"
@@ -53,6 +57,8 @@ func TestMultiDefine_charB1_session(t *testing.T) {
 
 func TestWorkload_charB1_session(t *testing.T) {
 	enableLogging(t)
+	revert := setC1Bool()
+	defer revert()
 	testWorkload(charB1, t)
 }
 
@@ -96,6 +102,7 @@ func TestMultiDefine_charB1Null_session(t *testing.T) {
 }
 
 func TestWorkload_charB1Null_session(t *testing.T) {
+	defer setC1Bool()()
 	testWorkload(charB1Null, t)
 }
 
@@ -191,4 +198,11 @@ func TestWorkload_charC1Null_session(t *testing.T) {
 
 func TestBindDefine_charC1Null_nil_session(t *testing.T) {
 	testBindDefine(nil, charC1Null, t, nil)
+}
+
+func setC1Bool() func() {
+	rs := ora.Cfg().Env.StmtCfg.Rset
+	old := rs.Char1()
+	rs.SetChar1(ora.OraB)
+	return func() { rs.SetChar1(old) }
 }

z_db_test.go
@@ -9,6 +9,8 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"gopkg.in/rana/ora.v3"
 )
 
 func Test_open_cursors_db(t *testing.T) {
@@ -266,19 +268,27 @@ func Test_nclobNull_string_db(t *testing.T) {
 }
 
 func Test_charB1_bool_true_db(t *testing.T) {
+	gct := ora.Cfg().Env.StmtCfg.Rset.Char1()
+	ora.Cfg().Env.StmtCfg.Rset.SetChar1(ora.OraB)
+	defer ora.Cfg().Env.StmtCfg.Rset.SetChar1(gct)
 	testBindDefineDB(gen_boolTrue(), t, charB1)
 }
 
 func Test_charB1Null_bool_true_db(t *testing.T) {
+	defer setC1Bool()()
 	testBindDefineDB(gen_boolTrue(), t, charB1Null)
 }
 
 func Test_charC1_bool_true_db(t *testing.T) {
 	//enableLogging(t)
+	gct := ora.Cfg().Env.StmtCfg.Rset.Char1()
+	ora.Cfg().Env.StmtCfg.Rset.SetChar1(ora.OraB)
+	defer ora.Cfg().Env.StmtCfg.Rset.SetChar1(gct)
 	testBindDefineDB(gen_boolTrue(), t, charC1)
 }
 
 func Test_charC1Null_bool_true_db(t *testing.T) {
+	defer setC1Bool()()
 	testBindDefineDB(gen_boolTrue(), t, charC1Null)
 }
 

z_oracle_test.go
@@ -477,8 +477,13 @@ func testWorkload(oct oracleColumnType, t *testing.T) {
 					expected[c] = gen_string()
 					gcts[c] = ora.S
 				case charB1, charB1Null, charC1, charC1Null:
-					expected[c] = gen_boolTrue()
-					gcts[c] = ora.B
+					if gct := ora.Cfg().Env.StmtCfg.Rset.Char1(); gct == ora.B || gct == ora.OraB {
+						expected[c] = gen_boolTrue()
+						gcts[c] = ora.B
+					} else {
+						expected[c] = gen_string()[:1]
+						gcts[c] = ora.S
+					}
 				case blob, blobNull, longRaw, longRawNull:
 					expected[c] = gen_bytes(9)
 					gcts[c] = ora.Bin
@@ -3459,10 +3464,6 @@ func TestSetDrvCfg(t *testing.T) {
 	var b bool
 	if err := testDb.QueryRow(qry).Scan(&b); err != nil {
 		t.Fatalf("%s: %v", qry, err)
-	}
-	t.Logf("B=%v", b)
-	if !b {
-		t.Errorf("got %t, awaited 'false'", b)
 	}
 
 	ora.Cfg().Env.StmtCfg.Rset.SetChar1(ora.S)

commit 9b2f79c153533e6bd9d38b1d6e01f71e9d0c38f3

fix Slice errors introduced by 6dd783f2

bndDateSlice.go
@@ -67,7 +67,7 @@ func (bnd *bndDateSlice) bind(values []time.Time, position int, stmt *Stmt) (ite
 	}
 	valueSz := C.ACTUAL_LENGTH_TYPE(C.sizeof_OCIDate)
 	for n, timeValue := range values {
-		arr := bnd.ociDates[n : n+1 : 1]
+		arr := bnd.ociDates[n : n+1 : n+1]
 		ociSetDateTime(&arr[0], timeValue)
 		bnd.alen[n] = valueSz
 	}

bndTimeSlice.go
@@ -74,7 +74,7 @@ func (bnd *bndTimeSlice) bind(values []time.Time, position int, stmt *Stmt) (ite
 			tz = zoneOffset(make([]byte, 0, 6), timeValue)
 			timezones[off] = tz
 		}
-		arr := bnd.ociDateTimes[n : n+1 : 1]
+		arr := bnd.ociDateTimes[n : n+1 : n+1]
 		if err := (&dateTimep{p: &arr[0]}).Set(bnd.stmt.ses.srv.env, timeValue); err != nil {
 			return iterations, err
 		}
@@ -163,7 +163,7 @@ func (bnd *bndTimeSlice) close() (err error) {
 	}()
 
 	for n := range bnd.ociDateTimes {
-		arr := bnd.ociDateTimes[n : n+1 : 1]
+		arr := bnd.ociDateTimes[n : n+1 : n+1]
 		C.OCIDescriptorFree(
 			unsafe.Pointer(&arr[0]),  //void     *descp,
 			C.OCI_DTYPE_TIMESTAMP_TZ) //ub4      type );

z_time_session_test.go
@@ -257,6 +257,7 @@ func TestBindSlice_time_timestampLtzP9Null_session(t *testing.T) {
 }
 
 func TestBindSlice_OraTime_timestampLtzP9Null_session(t *testing.T) {
+	enableLogging(t)
 	testBindDefine(gen_OraTimeSlice(true), timestampLtzP9Null, t, nil)
 }
 