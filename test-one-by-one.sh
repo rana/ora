d83c82c269c199070d716a285ccfe3fb08d97390
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-11-24 23:38:38 +0100

    test-one-by-one: time runs

--- test-one-by-one.sh
+++ test-one-by-one.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 n="${1:-10}"
 
+PRG=./ora.v4.test
+if time echo '' >/dev/null 2>&1; then
+	PRG="time $PRG"
+elif /usr/bin/time echo '' >/dev/null 2>&1; then
+	PRG="/usr/bin/time $PRG"
+fi
+
 go install -race && go test -race -c && \
 grep -h '^func Test' *_test.go|cut -c10-|cut -d'(' -f1 \
 | sort -R | xargs -n "$n" | sed -e 's/ /|/g' | \
 while read nm;
 do
+	echo ''
 	echo "$nm"
-	./ora.v4.test -test.run=$nm || break
+	$PRG -test.run=$nm || break
 done
 
