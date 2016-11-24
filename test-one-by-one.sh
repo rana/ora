482e30a8fb0b7077a9e338faa3e352a25fdbeaa4
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2016-11-24 15:29:43 +0100

    test-one-by-one: randomize order

--- test-one-by-one.sh
+++ test-one-by-one.sh
@@ -3,7 +3,8 @@ n="${1:-10}"
 
 go install -race && go test -race -c && \
 grep -h '^func Test' z_*.go|cut -c10-|cut -d'(' -f1 \
-| xargs -n "$n" | sed -e 's/ /|/g' | while read nm;
+| sort -R | xargs -n "$n" | sed -e 's/ /|/g' | \
+while read nm;
 do
 	echo "$nm"
 	./ora.v4.test -test.v -test.run=$nm || break
