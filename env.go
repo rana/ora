d456299f8cda006f12126f9e76530ea569496187
Tamás Gulácsi <tgulacsi78@gmail.com>
2017-03-10 20:11:03 +0100

    don't panic on empty StmtCfg in Env.OpenSrv
    Fixes #171.

--- env.go
+++ env.go
@@ -128,7 +128,10 @@ func (env *Env) Close() (err error) {
 // OpenSrv connects to an Oracle server returning a *Srv and possible error.
 func (env *Env) OpenSrv(cfg SrvCfg) (srv *Srv, err error) {
 	if cfg.IsZero() {
-		panic("Parameter 'cfg' may not be empty.")
+		cfg.StmtCfg = env.Cfg()
+		if cfg.IsZero() {
+			panic("Parameter 'cfg' may not be empty.")
+		}
 	}
 	defer func() {
 		if value := recover(); value != nil {
