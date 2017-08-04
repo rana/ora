a881a3fd2faa60685d5208a74dbe711cede16073
Tamás Gulácsi <tgulacsi78@gmail.com>
2017-08-04 10:09:18 +0200

    bump version

--- ora.go
+++ ora.go
@@ -27,7 +27,7 @@ const (
 
 	// The driver version sent to an Oracle server and visible in
 	// V$SESSION_CONNECT_INFO or GV$SESSION_CONNECT_INFO.
-	Version string = "v4.1.10"
+	Version string = "v4.1.11"
 )
 
 var _drv *Drv
