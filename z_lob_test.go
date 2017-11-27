b2b8d03bba7d4e80cd5f650512987e49004542a2
Tamás Gulácsi <tgulacsi78@gmail.com>
2017-11-27 19:33:39 +0100

    TestLobIssue237 for #237

--- z_lob_test.go
+++ z_lob_test.go
@@ -6,6 +6,8 @@ package ora_test
 
 import (
 	"bytes"
+	"context"
+	"database/sql"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -15,6 +17,7 @@ import (
 	"strings"
 	"sync"
 	"testing"
+	"time"
 
 	ora "gopkg.in/rana/ora.v4"
 )
@@ -856,4 +859,90 @@ func reverseString(s string) string {
 		j--
 	}
 	return string(runes)
+}
+
+func TestLobIssue237(t *testing.T) {
+	t.Parallel()
+	//defer tl.enableLogging(t)()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	ms, err := newMetricSet(ctx, testDb)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer ms.Close()
+
+	for i := 0; i < 100; i++ {
+		if err := ctx.Err(); err != nil {
+			break
+		}
+		events, err := ms.Fetch(ctx)
+		t.Log("events:", len(events))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func newMetricSet(ctx context.Context, db *sql.DB) (*metricSet, error) {
+	qry := "select /* metricset: sqlstats */ inst_id, sql_fulltext, last_active_time from gv$sqlstats WHERE ROWNUM < 11"
+	stmt, err := db.PrepareContext(ctx, qry)
+	if err != nil {
+		return nil, err
+	}
+
+	return &metricSet{
+		stmt: stmt,
+	}, nil
+}
+
+type metricSet struct {
+	stmt *sql.Stmt
+}
+
+func (m *metricSet) Close() error {
+	st := m.stmt
+	m.stmt = nil
+	if st == nil {
+		return nil
+	}
+	return st.Close()
+}
+
+// Fetch methods implements the data gathering and data conversion to the right format
+// It returns the event which is then forward to the output. In case of an error, a
+// descriptive error must be returned.
+func (m *metricSet) Fetch(ctx context.Context) ([]event, error) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	rows, err := m.stmt.QueryContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var events []event
+	var buf bytes.Buffer
+	for rows.Next() {
+		var e event
+		var lob *ora.Lob
+		if err := rows.Scan(&e.ID, &lob, &e.LastActive); err != nil {
+			return events, err
+		}
+		buf.Reset()
+		if _, err := io.Copy(&buf, lob); err != nil {
+			return events, err
+		}
+		e.Text = buf.String()
+		events = append(events, e)
+	}
+
+	return events, nil
+}
+
+type event struct {
+	ID         int64
+	Text       string
+	LastActive time.Time
 }
